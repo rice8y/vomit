0327aa45722c5c0ef7492b2565c9b661031930c7
rice8y <eitoy1128@mbr.nifty.com>
2025-09-02 06:02:40 +0900

    add build.lua

---
+++ build.lua
@@ -0,0 +1,9 @@
+module = "vomit"
+
+unpackfiles  = {"vomit.ins"}  
+typesetfiles = {"vomit.dtx"}
+checkengines = {"luatex"}
+stdengine    = "luatex"
+sourcefiles  = {"vomit.dtx", "vomit.ins"}
+installfiles = {"vomit.cls"}
+docfiles     = {"vomit.pdf"}
